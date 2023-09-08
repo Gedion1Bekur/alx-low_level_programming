b1d88c01d43ac91b2957ed365d9be59333848845
Gedion1Bekur <gedionbekur@gmail.com>
2023-09-07 19:30:20 -0700

    0x00helloworld

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
-/*
+/**
+ * main- print string std.
  *
- * main- print string std
- *
- * return (0)- if it success
+ * Return: 0 if success.
  */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return(0);
+return (0);
 
 }
